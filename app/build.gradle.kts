0490a62e17623b1658766f1d157278bd2865dfd2
rheactdev <rheact@tuta.com>
2026-07-04 23:48:04 -0400

    Update versionName to "0.0.1-alpha01" in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 36
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1-alpha01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
